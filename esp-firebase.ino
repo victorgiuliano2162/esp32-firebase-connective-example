385fdbdae122a91dac00015f2b807139ab941174
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-04-18 00:24:28 -0300

    adicionado sensor de luminosidade

--- esp-firebase.ino
+++ esp-firebase.ino
@@ -1,8 +1,8 @@
-#include <Arduino.h> 
+#include <Arduino.h>
 #include <WiFi.h>              
-#include <Firebase_ESP_Client.h> 
-#include "addons/TokenHelper.h" 
-#include "addons/RTDBHelper.h" 
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
 #include <DHT.h>
 
 #define WIFI_SSID "brisa-pitoco" 
@@ -11,18 +11,21 @@
 #define DATABASE_URL "https://esp32-umidty-sensor-default-rtdb.firebaseio.com/" 
 #define DHT_SENSOR_PIN 4 
 #define DHT_SENSOR_TYPE DHT11 
+#define PHOTO_SENSOR 32
 
 
 DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
+FirebaseJson json;
 
 unsigned long sendDataPrevMillis = 0;
 bool signupOK = false;                     //since we are doing an anonymous sign in 
 
 void setup(){
   Serial.begin(115200);
+  pinMode(PHOTO_SENSOR, INPUT_PULLUP);
   dht_sensor.begin();
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -67,17 +70,21 @@ String getFormattedTimestamp() {
   return String(timestamp);
 }
 
-FirebaseJson json;
+
 
 void loop(){
 
   float temperature = dht_sensor.readTemperature();
   float humidity = dht_sensor.readHumidity();
+  unsigned int sensorValue = analogRead(PHOTO_SENSOR);
+  float voltage = sensorValue * (3.3 / 4095.0); // Conversão para tensão (ESP32 usa 12 bits ADC)
+  float lux = voltage * 95.6; // Ajuste com um fator experimental (valor pode variar)
+
 
   
   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 300000 || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
-    
+    // Write an Int number on the database path test/int
     if (Firebase.RTDB.setFloat(&fbdo, "/sensors/temperature", temperature)){
       Serial.println("PASSED");
       Serial.println("PATH: " + fbdo.dataPath());
@@ -105,18 +112,25 @@ void loop(){
       Serial.print("Humidity : ");
       Serial.println(humidity);
     }
-
+    
     String timestamp = getFormattedTimestamp();
     json.set("temperature", temperature);
     json.set("humidity", humidity);
     json.set("timestamp", timestamp);
-    String newPath = "sensors/" + timestamp;
+    //String newPath = "sensors/" + timestamp;
     Firebase.RTDB.setString(&fbdo, "sensors/timestamp", timestamp);
-    Firebase.RTDB.pushJSON(&fbdo, newPath, &json);
+    //Firebase.RTDB.pushJSON(&fbdo, newPath, &json);
     if (Firebase.RTDB.pushJSON(&fbdo, "sensors_data/sensor1", &json)) {
-    Serial.println("Dados pushados com sucesso em: " + newPath);
-} else {
-    Serial.println("Erro ao push JSON: " + fbdo.errorReason());
-}
+      Serial.println("Dados pushados com sucesso em: sensors_data/sensor1");
+    } else {
+      Serial.println("Erro ao push JSON: " + fbdo.errorReason());
+    }
+    json.clear();
+    json.set("lux", lux);
+    json.set("timestamp", timestamp);
+    Firebase.RTDB.pushJSON(&fbdo, "sensors_data/sensor2", &json);
+    Serial.println(sensorValue);
+    Serial.print("Luminiscência: ");
+    Serial.println(lux);
   }
 }
